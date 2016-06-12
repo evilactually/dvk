030710e2f801642e6b381a92b2aae6ac67d626b6
Vlad Shcherbakov <vlad.shcherbakov@gmail.com>
2016-06-11 18:54:22 -0600

    Skeleton functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,49 @@
-pub fn vkCreateInstance() {
-	
+pub type vkCreateInstanceFn = Option<unsafe extern "stdcall" fn(pCreateInfo: *const VkInstanceCreateInfo, 
+                                                                pAllocator: *const VkAllocationCallbacks, 
+                                                                pInstance: *mut VkInstance)>;
+
+static mut vkCreateInstanceFn: vkCreateInstanceFn = None;
+
+#[derive(Debug)]
+//#[derive(Copy)]
+#[repr(C)]
+pub struct VkInstanceCreateInfo {
+    pub a: u32
+}
+
+#[derive(Debug)]
+//#[derive(Copy)]
+#[repr(C)]
+pub struct VkAllocationCallbacks {
+    pub a: u32
+}
+
+#[derive(Debug)]
+//#[derive(Copy)]
+#[repr(C)]
+pub struct VkInstance {
+    pub a: u32
+}
+
+unsafe fn load_vulkan_core() {
+
+}
+
+unsafe fn load_vulkan_khr_surface() {
+
+}
+
+unsafe fn load_vulkan_khr_win32() {
+
+}
+
+pub unsafe fn vkCreateInstance(pCreateInfo: *const VkInstanceCreateInfo, 
+                               pAllocator: *const VkAllocationCallbacks, 
+                               pInstance: *mut VkInstance) {
+    match vkCreateInstanceFn {
+        Some(f) => {f(pCreateInfo, pAllocator, pInstance);},
+        None => (),
+    }
 }
 
 #[cfg(test)]
